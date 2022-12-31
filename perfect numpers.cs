491c894ac41d3fead926d4f2654a164fd2cd6e05
omarelnaghia <121725409+omarelnaghia@users.noreply.github.com>
2022-12-31 17:24:05 +0200

    Add files via upload

---
+++ perfect numpers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public class perfect
+{
+    public static void Main()
+    {
+        int n, i, sum;
+        int fristnumber, end_number;
+
+        Console.WriteLine("find perfect numbers\n");
+        Console.WriteLine("_____");
+        Console.WriteLine(" enter the frist number: ");
+        fristnumber = int.Parse(Console.ReadLine());
+
+        Console.Write("enter the end number : ");
+        end_number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("perfect number betwen {0} and {1}", fristnumber, lastnumber);
+
+
+        for (n = frist_number; n <= end_number; n++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == n&&sum!=0)
+            {
+                Console.WriteLine(" the perfect numbers : " + n);
+
+            }
+
+
+        }
+        Console.ReadKey();
+
+    }
+}
